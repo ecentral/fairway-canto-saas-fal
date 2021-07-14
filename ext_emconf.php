9a0119253114c0348a436ed560385d35993ada76
Tim Schreiner <tim.schreiner@km2.de>
2021-07-14 10:58:11 +0200

    [TASK] Add extension base structure

---
+++ ext_emconf.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the "canto_saas_fal" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+$EM_CONF['canto_saas_fal'] = [
+    'title' => 'Canto SaaS FAL',
+    'description' => 'Adds Canto SaaS FAL driver.',
+    'category' => 'misc',
+    'version' => '0.1.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '10.4.0-11.3.99',
+        ],
+    ],
+    'state' => 'beta',
+    'clearCacheOnLoad' => true,
+    'author' => 'Tim Schreiner',
+    'author_email' => 'tim.schreiner@km2.de',
+    'author_company' => 'eCentral GmbH',
+];
