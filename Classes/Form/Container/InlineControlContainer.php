96af14a42a20c700dc2826ba27e009838f089599
Christian Rodriguez Benthake <c.benthake@ecentral.de>
2021-09-30 17:43:46 +0200

    Fixing the Add-By-Canto-Button upon Inline-Editing Backend-Requests
    When using FlexForms, images could not be added through canto because the ServerRequest did not have the returnUrl available at the correct spot
    JIRA: FAIRCANTO-46

--- Classes/Form/Container/InlineControlContainer.php
+++ Classes/Form/Container/InlineControlContainer.php
@@ -12,6 +12,7 @@ declare(strict_types=1);
 namespace Ecentral\CantoSaasFal\Form\Container;
 
 use Ecentral\CantoSaasFal\Resource\Driver\CantoDriver;
+use JsonException;
 use TYPO3\CMS\Backend\Routing\UriBuilder;
 use TYPO3\CMS\Core\Authentication\BackendUserAuthentication;
 use TYPO3\CMS\Core\Exception\SiteNotFoundException;
@@ -120,11 +121,40 @@ class InlineControlContainer extends \TYPO3\CMS\Backend\Form\Container\InlineCon
      */
     protected function getCurrentSite(): Site
     {
-        $returnUrl = $this->getTypo3Request()->getQueryParams()['returnUrl'] ?? '';
+        $site = $this->getTypo3Request()->getAttribute('site');
+        // In (Ajax) Backend Requests, this is NullSite
+        if ($site instanceof Site) {
+            return $site;
+        }
+        $ajax = $this->getTypo3Request()->getParsedBody()['ajax'] ?? $this->getTypo3Request()->getQueryParams()['ajax'];
+        $url = '';
+        if ($ajax['context'] !== null) {
+            $config = [];
+            try {
+                $context = json_decode($ajax['context'], true, 512, JSON_THROW_ON_ERROR);
+                $config = json_decode($context['config'], true, 512, JSON_THROW_ON_ERROR);
+            } catch (JsonException $exception) {
+            }
+            if ($config) {
+                $url = urldecode($config['originalReturnUrl'] ?? '');
+            }
+        }
+        // should the site attribute not be set for a normal Request, them we fall back to the SiteFinder-Method way
+        $returnUrl = $url ?: $this->getTypo3Request()->getQueryParams()['returnUrl'] ?? '';
         $queryString = parse_url($returnUrl, PHP_URL_QUERY) ?? '';
         parse_str($queryString, $queryParams);
-        $pageId = (int)$queryParams['id'] ?? 0;
+        $pageId = (int)$queryParams['id'];
         if ($returnUrl === '' || $pageId === 0) {
+            $allSites = $this->getSiteFinder()->getAllSites();
+            if (count($allSites) === 1) {
+                // this is the absolute last fallback if all other methods should fail
+                // that should guarantee that we always find on a single domain setup a SiteConfiguration
+                // if the SiteConfiguration has been set up and no PseudoSite is returned
+                $site = array_values($allSites)[0];
+                if ($site instanceof Site) {
+                    return $site;
+                }
+            }
             throw new SiteNotFoundException('Site configuration could not be determined.', 1628504403);
         }
         return $this->getSiteFinder()->getSiteByPageId($pageId);
