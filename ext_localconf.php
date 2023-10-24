b0186493903c40182467c60e46a181bb67716da6
denis <d.doerner@ecentral.de>
2023-10-24 18:05:44 +0200

    code beatify php-stan

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,10 +1,12 @@
 <?php
+
 /*
  * This file is part of the "canto_saas_fal" Extension for TYPO3 CMS.
  *
  * For the full copyright and license information, please read the
  * LICENSE file that was distributed with this source code.
  */
+
 use TYPO3\CMS\Core\Resource\Index\ExtractorRegistry;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
