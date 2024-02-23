29649acf539a08c4fd8563501c02c97611756e58
denis <d.doerner@ecentral.de>
2024-02-23 10:02:05 +0100

    Append Handle category mapping in EventListener from old mr and fix some code

--- Classes/Domain/Repository/FileReferenceRepository.php
+++ Classes/Domain/Repository/FileReferenceRepository.php
@@ -3,11 +3,12 @@
 declare(strict_types=1);
 
 /*
-* This file is part of the "canto_saas_fal" Extension for TYPO3 CMS.
-*
-* For the full copyright and license information, please read the
-* LICENSE file that was distributed with this source code.
-*/
+ * This file is part of the "canto_saas_fal" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
 namespace Fairway\CantoSaasFal\Domain\Repository;
 
 use TYPO3\CMS\Core\Database\ConnectionPool;
